c4e725774a49257e5e08165770f333e326332afb
Mainian <k032492@gmail.com>
2015-03-23 19:14:39 -0400

    First Drop
    Initial Drop

---
+++ Webcrawler/Crawler/Scrapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Webcrawler.Crawler
+{
+    public class Scrapper
+    {
+        public static async Task<string> GetWebsiteSourceCodeAsync(string url)
+        {
+            string sourceCode = "";
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                using(HttpWebResponse resp = (HttpWebResponse)await req.GetResponseAsync())
+                    using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                        sourceCode = await sr.ReadToEndAsync();
+            }
+            catch (WebException webException)
+            {
+                Console.WriteLine(webException.ToString());
+            }
+
+            return sourceCode;
+        }
+
+        public static string GetWebsiteSourceCodeSync(string url)
+        {
+            string sourceCode = "";
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+                    using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                        sourceCode = sr.ReadToEnd();
+            }
+            catch (WebException webException)
+            {
+                throw webException;
+            }
+
+            return sourceCode;
+        }
+
+        public static async Task<string> GetWebsiteSourceCodeWebClient(string url)
+        {
+            string getString = "";
+            WebClient client = new WebClient();
+            try
+            {
+                getString = await client.DownloadStringTaskAsync(url);
+            }
+            catch (WebException webException)
+            {
+                throw webException;
+            }
+
+            return getString;
+        }
+    }
+}
---
+++ Webcrawler/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webcrawler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            callCrawler("http://www.crossfitaoa.com/");
+        }
+
+        private async void callCrawler(string url)
+        {
+            string st = await Crawler.Scrapper.GetWebsiteSourceCodeAsync(url);
+            foreach (char c in st)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
